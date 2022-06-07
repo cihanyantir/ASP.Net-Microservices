b35950b7f0a6695fdc4b366dec18fe14fe7b9e51
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-06-07 18:48:12 +0300

    CatalogContext with MongoDB.Driver

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration) //it reaches setting.json
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString")); //by driver nuget
+            var database=client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));   //actually creating database
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:ConnectionName"));
+            CatalogContextSeed.SeedData(Products); //Bakılacak
+        }
+
+        public IMongoCollection<Product> Products { get; } //product collection
+    }
+}
