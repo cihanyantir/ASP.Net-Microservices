81680dda990a635cef8d333103483fecdc93bd3e
Cihan Yantır <cihan.yantir@ogr.sakarya.edu.tr>
2022-06-29 01:05:31 +0300

    gRPC services

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _repository;
+        private readonly ILogger _logger;
+        private readonly IMapper _mapper;
+
+        public DiscountService(IDiscountRepository repository, ILogger logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _repository.GetDiscount(request.ProductName);
+            if(coupon==null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName} is not found."));
+
+            }
+            _logger.LogInformation("Discountt is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+
+            var couponModel = _mapper.Map<CouponModel>(coupon);
+                return couponModel;
+        }
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var couponModel = _mapper.Map<Coupon>(request.Coupon); //repodaki create Coupon türünde istiyor ama sen CouponModel türünde veriyorsun.
+                                                                   // bu yüzden mapper kullanılıyor. request.coupon'u coupon türünde verdi.
+            _logger.LogInformation("Discountt is succesfully created : {productName}", couponModel.ProductName);
+            await _repository.CreateDiscount(couponModel);
+            var couponmodel2 = _mapper.Map<CouponModel>(couponModel);
+            return couponmodel2;
+                
+        }
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = _mapper.Map<Coupon>(request.Coupon);
+
+            await _repository.UpdateDiscount(coupon);
+            _logger.LogInformation("Discount is successfully updated. ProductName : {ProductName}", coupon.ProductName);
+
+            var couponModel = _mapper.Map<CouponModel>(coupon);
+            return couponModel;
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var deleted = await _repository.DeleteDiscount(request.ProductName);
+            var response = new DeleteDiscountResponse
+            {
+                Success = deleted //repositorydeki delete metodu boolean döndürüyor.
+            };
+
+            return response;
+        }
+    }
+}
