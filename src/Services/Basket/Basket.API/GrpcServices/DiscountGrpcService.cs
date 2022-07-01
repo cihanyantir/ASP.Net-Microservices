7f2ae1b994610f00241b72cb1e5926adcdaf57aa
Cihan Yantır <cihan.yantir@ogr.sakarya.edu.tr>
2022-07-01 21:29:52 +0300

    Reaching grpc methods with service (as Client)

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,31 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountprotoservice; 
+        // FROM Discount.Grpc
+        
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountprotoservice)
+        {
+            _discountprotoservice = discountprotoservice;
+        }
+        public async Task<CouponModel> GetDiscount (string productName)
+        {
+            var discountrequest = new GetDiscountRequest { ProductName = productName };
+            //GetDiscountRequest metod içindeki parametre. Bu parametreyle grpcden veri çekiyorsun.
+            //gRPC ile iletişim için bağlı hizmet ekledik. Böylece discountgrpe basketten erişebildik.
+            //Burası discount'un çekildği repository. Controllerda bunu işledin.
+            //Restful apilerdeki gibi apiden apiye veri transferini url ile yapamıyorsun. 
+           
+            return await _discountprotoservice.GetDiscountAsync(discountrequest);
+           
+        }
+    }
+}
