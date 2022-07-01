5a782233a517dda8046d71fa50e7bcb96a6e245e
Cihan Yantır <cihan.yantir@ogr.sakarya.edu.tr>
2022-07-01 21:40:28 +0300

    Persistince interfaces

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ordering.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    interface IOrderRepository: IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
