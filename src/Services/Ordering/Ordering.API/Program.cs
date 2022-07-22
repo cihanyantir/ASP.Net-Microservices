5566c3154c4d02a7d37eb9b503e05ff0782136ec
Cihan Yantır <cihan.yantir@ogr.sakarya.edu.tr>
2022-07-22 23:01:01 +0300

    Revert "Revert "Registration for startup.cs""
    This reverts commit b1b68eb09459a407a38349262d944fb0ee553049.

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ordering.API.Extensions;
+using Ordering.Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +17,14 @@ namespace Ordering.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                 .Build()
+                 .MigrateDatabase<OrderContext>((context, services) =>
+                 {
+                     var logger = services.GetService<ILogger<OrderContextSeed>>();
+                     OrderContextSeed.SeedAsync(context, logger).Wait();
+                 })
+                 .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
