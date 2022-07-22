84df17105ed22d8e1f7db05490828b28081f69eb
Cihan Yantır <cihan.yantir@ogr.sakarya.edu.tr>
2022-07-22 22:41:06 +0300

    interface

--- src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Contracts.Persistence
 {
-    interface IOrderRepository: IAsyncRepository<Order>
+   public interface IOrderRepository: IAsyncRepository<Order>
     {
         Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
     }
