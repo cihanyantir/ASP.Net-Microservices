7b9d13bf80703548b17197ac77a453b09a23a443
Cihan Yantır <cihan.yantir@ogr.sakarya.edu.tr>
2022-07-22 22:43:22 +0300

    Repos

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Common;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+   public class OrderRepository : RepositoryBase<Order> ,IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderlist = await _dbContext.Orders
+                 .Where(x => x.UserName == userName).ToListAsync();
+            return orderlist;
+        }
+    }
+}
