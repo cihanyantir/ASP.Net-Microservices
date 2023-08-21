96b0f9f4553c2a5d4f6b64e41de4dc2c53c4dff7
Cihan Yantır <cihan.yantir@ogr.sakarya.edu.tr>
2023-08-21 15:07:19 +0300

    package, transit settings

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -45,12 +45,14 @@ namespace Basket.API
             {
                 cfg.Host(Configuration["EventBusSettings:HostAddress"]);
 
-            }
-            );
             });
-            
-
-
+            });
+            services.Configure<MassTransitHostOptions>(options =>
+            {
+                options.WaitUntilStarted = true;
+                options.StartTimeout = TimeSpan.FromSeconds(30);
+                options.StopTimeout = TimeSpan.FromMinutes(1);
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
